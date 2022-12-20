b5bef5350185987e3c5eec6558eb4309efa6721e
Karn Sandhu <karnsandhu2006@gmail.com>
2022-12-19 16:57:03 -0800

    initial commit

---
+++ website.py
@@ -0,0 +1,79 @@
+from PIL import Image
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
+
+lottie_coding = load_lottieurl("https://assets10.lottiefiles.com/packages/lf20_iv4dsx3q.json")
+img_contact_form = Image.open("Images/hero.png")
+
+with st.container():
+    st.subheader("Hello")
+    st.title("High school student")
+    st.write("my name is karn")
+
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+            Information--
+            - more
+            - more
+            - even more
+
+            """
+        )
+
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
+
+with st.container():
+    st.write("---")
+    st.header("Section 2")
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_contact_form)
+    with text_column:
+        st.subheader("Something something..")
+        st.write("asdfdsaf")
+
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
+    contact_form = """
+    <form action="https://formsubmit.co/karnsandhu2006@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Your name" required>
+    <input type="email" name="email" placeholder="Your email" required>
+    <textarea name="message" placeholder="Your message here" required></textarea>
+    <button type="submit">Send</button>
+    </form>
+    """
+
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
